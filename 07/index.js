7b26fdd329e5d15707a912fc530c9310d26a0293
Tom Corke <tom.corke@skyscanner.net>
2017-12-07 12:44:48 +0000

    Performance timing day 7

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { performance } = require('perf_hooks');
 
 const testInputString = fs.readFileSync(path.resolve(__dirname, 'testInput.txt'), 'utf8');
 const inputString = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
@@ -37,22 +38,28 @@ console.log(findBottom(input));
 
 function findUnbalanced(input) {
 
+  const start = performance.now();
+
   const mappedInput = input.reduce((items, item) => Object.assign({}, items, {[item.name]: item}), {});
 
   function getTotalWeight(name) {
-    return mappedInput[name].weight + mappedInput[name].children.reduce((total, child) => total + getTotalWeight(child), 0);
+    const item = mappedInput[name];
+    if (item.totalWeight) { return item.totalWeight; }
+    const totalWeight = item.weight + item.children.reduce((total, child) => total + getTotalWeight(child), 0);
+    item.totalWeight = totalWeight;
+    return totalWeight;
   }
 
   function testWeight(name) {
-    if (!mappedInput[name].children.every(child => testWeight(child))) {
+    if (mappedInput[name].children.some(child => !testWeight(child))) {
       return false;
     }
 
     const childWeights = mappedInput[name].children.map(child => ({ name: child, weight: getTotalWeight(child) }));
     if (new Set(childWeights.map(child => child.weight)).size > 1) {
-      console.log(`Mismatched child weights at ${name}: ${JSON.stringify(childWeights)}`);
+      console.log(`Mismatched child weights at ${name}`);
       console.log('Children:');
-      mappedInput[name].children.forEach(child => console.log(`${child}: ${mappedInput[child].weight} => ${JSON.stringify(mappedInput[child].children.map(getTotalWeight))}`))
+      mappedInput[name].children.forEach(child => console.log(`${child} (${childWeights.find(cw => cw.name === child).weight}): ${mappedInput[child].weight} => ${JSON.stringify(mappedInput[child].children.map(getTotalWeight))}`))
       return false;
     }
 
@@ -61,6 +68,11 @@ function findUnbalanced(input) {
 
   const bottom = findBottom(input);
   testWeight(bottom.name);
+
+  const end = performance.now();
+  const duration = Math.floor((end - start) * 10) / 10;
+
+  console.log(`Time taken: ${duration}ms`);
 
 }
 
