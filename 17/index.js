90a81ba01120b152b206da9f692e8fe06d27341d
Tom Corke <tom@tomcorke.com>
2017-12-17 14:09:04 +0000

    Show both runs in output for day 17 at once, with timings

--- 17/index.js
+++ 17/index.js
@@ -1,5 +1,6 @@
+const { time } = require('../util');
 
-function spinlock(initialBuffer, input, iterations = 2017) {
+function spinlock(initialBuffer, input, iterations = 2017, skipBufferOps = false) {
     let buffer = initialBuffer.slice();
     
     let position = 0;
@@ -7,15 +8,17 @@ function spinlock(initialBuffer, input, iterations = 2017) {
     function spin(n) {
         position = ((position + input) % n) + 1
         if (position == 1) { console.log(`Insert at position 1: ${n}`); }
-        // buffer.splice(position, 0, n);
+        if (!skipBufferOps) { buffer.splice(position, 0, n); }
     }
 
     for(let i = 1; i <= iterations; i++) {
         spin(i);
     }
 
-    console.log(buffer[position + 1]);
-    console.log(buffer[1]);
+    if (!skipBufferOps) {
+        console.log(buffer[position + 1]);
+    }
 }
 
-spinlock([0], 356, 50000000);
+time(() => spinlock([0], 356));
+time(() => spinlock([0], 356, 50000000, true));
