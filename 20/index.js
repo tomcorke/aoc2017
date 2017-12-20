b5c3e69e9a1fb341fb1a899f2c188cb0e2b2c7be
Tom Corke <tom@tomcorke.com>
2017-12-20 07:57:09 +0000

    More experimentation

--- 20/index.js
+++ 20/index.js
@@ -46,17 +46,35 @@ function parse(line, index) {
 
 const input = readFile('20/input.txt').split('\n').map(parse);
 
+function sign(n) {
+    return n < 0 ? -1 : 1;
+}
+
 function run(input) {
+    
+/*
     while (!input.every(particle => !particle.movedCloser)) {
         input.forEach(particle => particle.move());
     }
+*/
+
+    console.log(Math.min(...input.map(p => p.index)), Math.max(...input.map(p => p.index)));
+
     const sortedByMinimumDistance = input.sort((a, b) => a.minimumDist - b.minimumDist);
     const closest = sortedByMinimumDistance[0];
-    console.log(closest);
-
+    
     const sortedByAcceleration = input.sort((a, b) => a.a.magnitude - b.a.magnitude);
-    const slowest = sortedByAcceleration[0];
-    console.log(slowest);
+    const slowestAcceleration = sortedByAcceleration[0].a.magnitude;
+
+    const jointSlowestAcceleration = input.filter(p => p.a.magnitude === slowestAcceleration);
+    const slowestSlowest = jointSlowestAcceleration.sort((a, b) => a.v.magnitude - b.v.magnitude);
+    const slowestAccelerationVelocity = slowestSlowest[0].v.magnitude;
+
+    const jointSlowestAccelerationVelocity = input.filter(p => p.v.magnitude === slowestAccelerationVelocity);
+    const slowestSlowestClosest = jointSlowestAccelerationVelocity.sort((a, b) => a.p.magnitude - b.p.magnitude);
+    
+
+    console.log(sortedByAcceleration.slice(0, 5));
 }
 
 run(input);
