98d4b8885d2a5f92c8a83454128b8cb0e6d59906
Tom Corke <tom@tomcorke.com>
2017-12-24 00:47:02 +0000

    23 solved, finally!

--- 23/part2.js
+++ 23/part2.js
@@ -1,46 +1,35 @@
-let a = 0;
-let n = 0;
-let max = 0;
-let x = 0
-let y = 0
-let g = 0
+let a = 1;
 
-let countPrimes = 0
+let nonPrimes = 0
 
 let startn = 0;
+let max = 0;
 
 if (a != 0) {
   startn = 99 * 100 + 100000
   max = startn + 17000
-  console.log(startn, max)
 } else {
-  startn = 97
-  max = 200
+  startn = 99
+  max = 99
 }
 
-for (let n = startn; n < max; n += 17) {
+for (let n = startn; n <= max; n += 17) {
 
   let isPrime = true
 
-  outer_loop:
+  for (let x = 2; x < n; x++) {
 
-  for (x = 2; x < n; x++) {
-
-    for(y = 2; y < n; y++) {
-
-      if (x * y == n) {
-        isPrime = false
-        break outer_loop;
-      }
-
+    if (n % x == 0) {
+      isPrime = false
+      break;
     }
 
   }
 
-  if (isPrime == true) {
-    countPrimes += 1
+  if (isPrime == false) {
+    nonPrimes += 1
   }
 
 }
 
-console.log(countPrimes);
+console.log(nonPrimes);
